6aca9849f42beef086e2d56a7eba20547994d10b
Alex <ayaroslavskiy91@gmail.com>
2024-12-01 13:36:50 -0500

    added contextupdater

--- extensions.py
+++ extensions.py
@@ -4,3 +4,9 @@ Custom extensions.
 
 from copier_template_extensions import ContextHook
 from jinja2.ext import Extension
+
+
+class ContextUpdater(ContextHook):
+    """
+    Inplace updater of Jinja Context
+    """
